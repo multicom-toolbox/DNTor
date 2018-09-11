9fb78499eadd977dca628ef26e588112cbeb385b
farhanquadir <farhanquadir1983@gmail.com>
2018-09-11 17:14:51 -0500

    removed wrong additions

--- bin/DeepQA.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#DeepQA prediction file for protein single-model quality assessment #
-if [ $# -ne 3 ]
-then
-	echo "need three parameters : path of fasta sequence, directory of input pdbs, directory of output"
-	exit 1
-fi
-perl /home/jh7x3/tools/DeepQA/script/DeepQA.pl /home/jh7x3/tools/DeepQA/script /home/jh7x3/tools/DeepQA/tools $1 $2 $3
